869bebef184e72076d993ece4b3d50e55c78fe6c
Stanislav Matsak <stanislav.matsak@nure.ua>
2023-12-08 03:04:20 +0200

    Encrypted email and password input fields => textarea

--- src/components/info-container/info-container.jsx
+++ src/components/info-container/info-container.jsx
@@ -6,21 +6,23 @@ const InfoContainer = ({encryptedEmail, encryptedPassword, receivedPassword, rec
             <label htmlFor="emailEncypted"
                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-left">Encrypted
                 email</label>
-            <input type="email" id="emailEncypted" aria-describedby="helper-text-explanation"
+            <textarea id="emailEncypted" aria-describedby="helper-text-explanation"
                    value={encryptedEmail ? encryptedEmail : null}
+                      cols={2}
                    className="bg-gray-50 cursor-not-allowed border border-gray-300 text-gray-900 text-sm rounded-lg
                    focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 shadow-md shadow-blue-800
-                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 resize-none"
                    disabled/>
 
             <label htmlFor="passwordEncrypted"
                    className="block mt-8 mb-2 text-sm font-medium text-gray-900 dark:text-white text-left">Encrypted
                 password</label>
-            <input type="text" id="passwordEncrypted" aria-describedby="helper-text-explanation"
+            <textarea type="text" id="passwordEncrypted" aria-describedby="helper-text-explanation"
                    value={encryptedPassword ? encryptedPassword : null}
-                   className="bg-gray-50 cursor-not-allowed border border-gray-300 text-gray-900 text-sm rounded-lg
+                   cols={2}
+                      className="bg-gray-50 cursor-not-allowed border border-gray-300 text-gray-900 text-sm rounded-lg
                    focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 shadow-md shadow-blue-800
-                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 resize-none"
                    disabled/>
 
             <div className="flex flex-row items-center w-full">
